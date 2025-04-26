88b9701a66626eb0ad87b09e8f0d6888085a29f5
eunseo0302 <eunseo03@gmail.com>
2025-04-26 16:18:15 +0900

    Create function_input.cpp

---
+++ 01_basic_syntax/function_input.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+int getInput() {
+    int input = 0;
+    printf("숫자를 입력해주세요: ");
+    fseek(stdin, 0, SEEK_END);
+    scanf("%d", &input);
+    return input;
+}
+
+int main() {
+    int input = getInput();
+    
+    if (input <= 0) {
+        printf("숫자를 다시 입력해주세요\n");
+    } else {
+        for (int i = 0; i < input; i++) {
+            printf("A");
+        }
+        printf("\n");
+    }
+
+    input = getInput();
+
+    if (input <= 0) {
+        printf("숫자를 다시 입력해주세요\n");
+    } else {
+        for (int i = 0; i < input; i++) {
+            printf("B");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+Add function-based input printing code
