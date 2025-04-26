5568aac2363295dbcc2e4360eb54a903b098190b
eunseo0302 <eunseo03@gmail.com>
2025-04-26 16:22:27 +0900

    Create calculator_switch.cpp

---
+++ 01_basic_syntax/calculator_switch.cpp
@@ -0,0 +1,62 @@
+#include <cstdio>
+
+float getinput();
+char getChar();
+
+int main() {
+    
+    float input1 = getinput();
+    float input2 = getinput();
+    char cal = getChar();
+    
+    
+    switch(cal) {
+        case '+':
+            printf("%.3f", input1 + input2);
+            break;
+        case '-':
+            printf("%.3f", input1 - input2);
+            break;
+        case '*':
+            printf("%.3f", input1 * input2);
+            break;
+        case '/':
+        if (input2 == 0) {
+            printf("오류입니다");
+        }
+        else {
+            printf("%.3f", input1 / input2);
+        }
+            break;
+        case '%':
+            float result;
+            result = (int)input1 % (int)input2;
+            printf("%.3f", result);
+            break;
+            
+        default:
+            printf("오류입니다");
+            break;
+        
+    }
+    
+    return 0;
+}
+
+float getinput() {
+    float a;
+    printf("입력하세요:");
+    fseek(stdin, 0, SEEK_END);
+    scanf("%f", &a);
+    return a;
+}
+
+char getChar() {
+    char p;
+    printf("연산자를 입력하세요:");
+    fseek(stdin, 0, SEEK_END);
+    scanf(" %c", &p);
+    return p;
+}
+
+Add basic calculator using functions and switch statement
