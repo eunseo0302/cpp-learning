2dcb14c20603b1ae90eadec84fb00689a2c3175f
eunseo0302 <eunseo03@gmail.com>
2025-04-26 16:42:06 +0900

    Create reverse_string.cpp

---
+++ 01_basic_syntax/reverse_string.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+int main() {
+    
+    char input[100];
+    printf("문자를 입력하세요:");
+    fseek(stdin, 0, SEEK_END);
+    scanf("%99[^\n]", input);
+    
+    
+    int index;
+    
+    for (int i = 0; ; i++) {
+        if (input[i] == '\0') {
+            index = i;
+            break;
+        }
+    }
+    
+    for (int i = index - 1; i >= 0 ; i--) {
+        printf("%c", input[i]);
+    }
+    
+    
+    return 0;
+}
+
+Add reverse string program using manual null-terminator search
